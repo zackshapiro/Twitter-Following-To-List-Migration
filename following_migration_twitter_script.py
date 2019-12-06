136fe40b2fef84d8ae719c6e76c623b430ba7eec
Zack Shapiro <zack@zackshapiro.com>
2019-12-06 12:11:32 -0500

    get around rate limit

--- following_migration_twitter_script.py
+++ following_migration_twitter_script.py
@@ -37,20 +37,23 @@ following_count = len(api.friends_ids())
 num_added_to_list = 0
 #Iterate through friends adding them to list
 for friend_id in api.friends_ids():
+    print(f"adding {friend_id} to list")
     api.add_list_member(list_id=our_list.id, user_id=friend_id, owner_id=api.me().id)
     num_added_to_list += 1
+    api.destroy_friendship(friend_id)
+    print(f"unfollowing {friend_id}")
 
-#Verify that every person we're following is added to list
-if num_added_to_list == following_count:
-    verified = True
-else:
-    verified = False
+# #Verify that every person we're following is added to list
+# if num_added_to_list == following_count:
+#     verified = True
+# else:
+#     verified = False
 
-if verified:
-    #Iterate through friends unfollowing them
-    for friend_id in api.friends_ids():
-        api.destroy_friendship(friend_id)
-    print('Script has finished running successfully.')
-else:
-    print("ERROR - Not all follows were added to list, not unfollowing users.")
-    print('Script has been terminated due to error.')
+# if verified:
+#     #Iterate through friends unfollowing them
+#     for friend_id in api.friends_ids():
+#         api.destroy_friendship(friend_id)
+#     print('Script has finished running successfully.')
+# else:
+#     print("ERROR - Not all follows were added to list, not unfollowing users.")
+#     print('Script has been terminated due to error.')
